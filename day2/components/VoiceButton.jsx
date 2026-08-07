6a91dc04e5421ff6bee5a98f9ef517d59d085c5c
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2026-08-07 19:59:21 +0530

    Create VoiceButton.jsx

---
+++ day2/components/VoiceButton.jsx
@@ -0,0 +1,21 @@
+function VoiceButton(){
+
+    return(
+
+        <div className="card center">
+
+            <h2>🎤 Voice Conversation</h2>
+
+            <button>
+
+                Start Speaking
+
+            </button>
+
+        </div>
+
+    )
+
+}
+
+export default VoiceButton;
