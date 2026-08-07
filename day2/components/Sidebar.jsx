74f755171dae36e4385d7003bfe8988118aba862
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2026-08-07 19:58:28 +0530

    Create Sidebar.jsx

---
+++ day2/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+function Sidebar() {
+
+    return (
+
+        <div className="sidebar">
+
+            <h2>🎓 ShikshaSaathi</h2>
+
+            <p>Learning & Literacy</p>
+
+            <hr />
+
+            <ul>
+
+                <li>🏠 Home</li>
+
+                <li>🎤 Voice Chat</li>
+
+                <li>📚 Learn</li>
+
+                <li>📈 Progress</li>
+
+                <li>⚙ Settings</li>
+
+            </ul>
+
+            <div className="murf">
+
+                Powered by
+
+                <h3>Murf Falcon</h3>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default Sidebar;
