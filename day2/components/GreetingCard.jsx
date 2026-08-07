079edc0f89bbd14e01c6452303fc7e671317c0b9
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2026-08-07 19:58:46 +0530

    Create GreetingCard.jsx

---
+++ day2/components/GreetingCard.jsx
@@ -0,0 +1,35 @@
+function GreetingCard(){
+
+    return(
+
+        <div className="card">
+
+            <h1>Hello 👋</h1>
+
+            <h3>I am ShikshaSaathi</h3>
+
+            <p>
+
+                Your AI Voice Tutor.
+
+                I explain concepts step by step,
+
+                encourage learning,
+
+                and never judge wrong answers.
+
+            </p>
+
+            <span className="online">
+
+                🟢 Agent Online
+
+            </span>
+
+        </div>
+
+    )
+
+}
+
+export default GreetingCard;
