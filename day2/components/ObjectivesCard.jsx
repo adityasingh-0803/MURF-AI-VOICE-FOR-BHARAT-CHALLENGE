b1adc5de8a6bf8e7a7659b4024ddb87f338ed219
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2026-08-07 20:00:03 +0530

    Create ObjectivesCard.jsx

---
+++ day2/components/ObjectivesCard.jsx
@@ -0,0 +1,25 @@
+function ObjectivesCard(){
+
+    return(
+
+        <div className="card">
+
+            <h2>🎯 Objectives</h2>
+
+            <ul>
+
+                <li>Explain concepts clearly.</li>
+
+                <li>Encourage curiosity.</li>
+
+                <li>Improve confidence.</li>
+
+            </ul>
+
+        </div>
+
+    )
+
+}
+
+export default ObjectivesCard;
